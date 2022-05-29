83a52c8d0294bd224075c3d962bbfb49d5278619
Philip Holtzman <phil.holtzman@gmail.com>
2022-05-30 00:09:08 +0300

    i fucked it all up at the end

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let hue = 0;
+
 const particlesArray = [];
 
 
@@ -22,58 +24,77 @@ const mouse = {
 canvas.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    //drawCircle();
+    for (i = 0; i < 20; i++){
+        particlesArray.push(new Particle());
+
+    };
+    
 });
 
 canvas.addEventListener('mousemove', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    //drawCircle();
+    for (i = 0; i < 1; i++){
+        particlesArray.push(new Particle());
+
+    };
 });
 
 
 class Particle {
     constructor(){
-        //this.x = mouse.x;
-        //this.y = mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        //this.x = Math.random() * canvas.width;
+        //this.y = Math.random() * canvas.height;
         this.size = Math.random() * 5+1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
+        this.color = 'hsl(' + hue + ', 100%, 50%';
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
+        if(this.size > 0.2) this.size -= 0.05; //shrink ums
     }
     draw(){
-        ctx.fillStyle = '#9bc4bc';
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x,this.y, this.size, 0, Math.PI *2);
         ctx.fill();
     }
 }
 
-function init(){ // create an array of 100 particles
-    for (let i = 0; i < 100; i++){
-        particlesArray.push(new Particle());
-    }
-}
 
 function handleParticles(){
     for (let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        
+    }   for (let j = i; j < particlesArray.length; j++ ){
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+    }
+    if (particlesArray[i].size <= 0.3){
+        particlesArray.splice(i,1);
+        i--;
     }
 }
 
-init();
+
 
 
 function animate(){
+
+
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    //ctx.fillStyle = 'rgba(0,0,0,0.02)'
+    //ctx.fillRect(0,0,canvas.width,canvas.height);
     handleParticles();
+    hue+=2;
     requestAnimationFrame(animate);
+
 };
 
 animate();
